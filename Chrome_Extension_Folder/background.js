05cf189ac17868a94c01741701d823aeaff1df70
kartikdiwan <71651448+kartikdiwan@users.noreply.github.com>
2023-05-30 13:26:40 +0530

    Add files via upload

--- Chrome_Extension_Folder/background.js
+++ Chrome_Extension_Folder/background.js
@@ -1,23 +1,23 @@
 function send_playlist()
 {
-    var label = document.createElement("label");
-    label.innerHTML = "Downloading...";
-    document.getElementById("downloading").appendChild(label);
+    var p = document.getElementById("downloading");
+    p.innerHTML = "Downloading...";
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
-        $.ajax({
-            headers: { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-            },
-            'type': 'POST',
-            'url': "http://localhost:8008/",
-            'data': JSON.stringify(tabs[0].url),
-            'dataType': 'json',
-            success: function(recieved_data){
-                var label = document.createElement("label");
-                label.innerHTML = recieved_data;
-                document.getElementById("downloads").appendChild(label);
-            }
+        $(document).ready(function() {
+            $.ajax({
+                headers: { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                'type': 'POST',
+                'url': "http://localhost:8008/",
+                'data': JSON.stringify(tabs[0].url),
+                'dataType': 'json',
+                success: function(recieved_data){
+                    var p = document.getElementById("downloading");
+                    p.innerHTML = recieved_data;
+                }
+            });
         });
     });
     
